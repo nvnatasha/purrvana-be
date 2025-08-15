1cf9959b8476788b2bedfd71430828031e95c657
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2025-08-15 18:25:28 -0500

    changes to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
 allow do
-    origins "*"
+    origins 'http://localhost:5173', 'https://your-frontend-url.onrender.com'
+
 
     resource "*",
         headers: :any,
+        expose: ['Authorization'],
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
 end
